954049c8ba066ab863f3df0709d5fe0dacd04af2
Topu492 <topu86702@gmail.com>
2025-05-12 11:55:08 +0600

    php project

---
+++ resources/config.php
@@ -0,0 +1,21 @@
+<?php
+
+ob_start();
+session_start();
+
+defined("DS") ? null : define("DS",DIRECTORY_SEPARATOR);
+
+defined("Template_frontend") ? null : define("Template_frontend", __DIR__ . DS . "templates/frontend");
+defined("Template_backend") ? null : define("Template_backend", __DIR__ . DS . "templates/backend");
+//echo __DIR__;
+
+defined("DB_HOST") ? null : define("DB_HOST","localhost");
+defined("DB_USER") ? null : define("DB_USER","root");
+defined("DB_PASS") ? null : define("DB_PASS","");
+defined("DB_NAME") ? null : define("DB_NAME","ecom_db");
+
+$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+require_once("functions.php");
+
+
+?>
