6b236af2b4da9fcbbc92e24de378876e0f077689
Topu492 <topu86702@gmail.com>
2025-05-18 18:17:19 +0600

    first commit

---
+++ public/shop.php
@@ -0,0 +1,27 @@
+<?php
+require_once("../resources/config.php");?>
+<?php  include(Template_frontend . DS . "header.php") ?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Jumbotron Header -->
+        <header>
+          <h1>Shop</h1> 
+        </header>
+
+        <hr>
+
+        <!-- Page Features -->
+        <div class="row text-center">
+           
+        <?php  get_products_shop_in_page()  ?>    
+        
+        </div>
+        <!-- /.row -->
+
+
+    </div>
+    <!-- /.container -->
+
+<?php  include(Template_frontend . DS . "footer.php") ?>
--- resources/functions.php
+++ resources/functions.php
@@ -1,4 +1,136 @@
 <?php
 
+// helper functions
+
+function redirect($location){
+    header("Location: $location");
+}
+
+
+function query($sql){
+    global $connection;
+
+    return mysqli_query($connection,$sql);
+}
+
+function confirm($reqult){
+    global $connection;
+
+     if(!$reqult){
+      die("Query Failed". mysqli_error($connection));
+    }
+}
+
+function escape_string($string){
+    global $connection;
+    return mysqli_real_escape_string($connection,$string);
+}
+
+function fetch_array($result){
+    return mysqli_fetch_array($result);
+}
+
+
+//******************************************FrontEnd Function ***************************************/
+
+// get products
+
+function get_products(){
+    $query = query("SELECT * FROM products");
+    confirm($query);
+    while($row = fetch_array($query)){
+$products = <<<DELIMETER
+
+        <div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                           <a href="item.php?id={$row['product_id']}"> </a> <img src="{$row['product_image']}" alt=""> </a>
+                            <div class="caption">
+                                <h4 class="pull-right">&#36;{$row['product_price']}</h4>
+                                <h4><a href="item.php?id={$row['product_id']}">{$row['product_title']}</a>
+                                </h4>
+                                <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
+                            <a class="btn btn-primary" target="_blank" href="item.php?id={$row['product_id']}">Add to cart</a>
+                            </div>
+                        </div>
+                    </div>
+
+DELIMETER;
+        echo $products;
+    }
+
+}
+
+// get categories
+
+ function get_categories(){
+$query = query("SELECT * FROM categories");
+confirm($query);
+while($row = fetch_array($query)){
+$categories = <<<DELIMETER
+<a href='category.php?id={$row['id']}' class='list-group-item'>{$row['cat_title']}</a>
+
+DELIMETER;   
+echo $categories;  
+
+ }
+
+}
+
+
+function get_products_cat_in_page(){
+    $query = query("SELECT * FROM products WHERE product_category_id = " . escape_string($_GET['id']) . " ");
+    confirm($query);
+    while($row = fetch_array($query)){
+$products = <<<DELIMETER
+
+       <div class="col-md-3 col-sm-6 hero-feature">
+                <div class="thumbnail">
+                    <img src="{$row['product_image']}" alt="">
+                    <div class="caption">
+                        <h3>{$row['product_title']}</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <p>
+                            <a href="#" class="btn btn-primary">Buy Now!</a> <a href="item.php?id={$row['product_id']}" class="btn btn-default">More Info</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+DELIMETER;
+        echo $products;
+    }
+
+}
+
+
+function get_products_shop_in_page(){
+    $query = query("SELECT * FROM products");
+    confirm($query);
+    while($row = fetch_array($query)){
+$products = <<<DELIMETER
+
+       <div class="col-md-3 col-sm-6 hero-feature">
+                <div class="thumbnail">
+                    <img src="{$row['product_image']}" alt="">
+                    <div class="caption">
+                        <h3>{$row['product_title']}</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <p>
+                            <a href="#" class="btn btn-primary">Buy Now!</a> <a href="item.php?id={$row['product_id']}" class="btn btn-default">More Info</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+DELIMETER;
+        echo $products;
+    }
+
+}
+
+
+
+//******************************************BackEnd Function *********************************/
+
+
+
 
 ?>
