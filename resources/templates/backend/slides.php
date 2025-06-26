e3cef7ac4e5b06b3ff8e1c17d5df0fe1ade434f3
Topu492 <topu86702@gmail.com>
2025-06-26 22:47:21 +0600

    slider page

---
+++ resources/templates/backend/slides.php
@@ -0,0 +1,27 @@
+<?php  add_slides();   ?>
+<div class="col-md-12">
+    <div class="row">
+        <h1 class="page-header">
+            Add user
+        </h1>
+    </div>
+   
+    <form action="" method="post" enctype="multipart/form-data">
+        <div class="col-md-8">
+             <div class="form-group">
+                <input type="file" name="file" class="form-control">
+            </div> 
+            <div class="form-group">
+                <label for="slide_title">Slide Title</label>
+                <input type="text" name="slide_title" class="form-control">
+            </div>        
+            <div class="form-group">
+                <input name="add_slide" type="submit" class="btn btn-primary" value="Slide">
+            </div>
+
+        </div><!--Main Content-->
+
+
+    </form>
+
+    <h2>Slide Available</h2>
